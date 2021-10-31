43c6dc17c55a1c556ebf3a4a661c18c593aa4384
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-10-31 10:49:11 +0200

    constants to connect database

---
+++ ClientServerWeather/src/dev/obniavka/dbconnection/Configs.java
@@ -0,0 +1,9 @@
+package dev.obniavka.dbconnection;
+
+public class Configs {
+    protected String dbHost = "127.0.0.1";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "obniavka123#";
+    protected String dbName = "weather";
+}
