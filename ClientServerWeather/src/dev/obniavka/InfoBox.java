6a888e9ca9abf5d5780c4d072ec89f96caebf2f6
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-10-31 17:24:04 +0200

    infoBox on login

--- ClientServerWeather/src/dev/obniavka/InfoBox.java
+++ ClientServerWeather/src/dev/obniavka/InfoBox.java
@@ -3,4 +3,8 @@ import javax.swing.*;
 
 public class InfoBox{
     public void incorrectCityMessage(){ JOptionPane.showMessageDialog(null,"Неправильно введене місто"); }
+
+    public void incorrectAdmin(){
+        JOptionPane.showMessageDialog(null,"Неправильно введений логін або пароль");
+    }
 }
