5674441328e9bc4f51d4fdc8d8c44fe0f00e05b1
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-01 09:19:44 +0200

    adding two message dialogs to show if weather info wasn't added correctly or it was saved into database

--- ClientServerWeather/src/dev/obniavka/InfoBox.java
+++ ClientServerWeather/src/dev/obniavka/InfoBox.java
@@ -7,4 +7,9 @@ public class InfoBox{
     public void incorrectAdmin(){
         JOptionPane.showMessageDialog(null,"Неправильно введений логін або пароль");
     }
+
+    public void weatherAdd(){ JOptionPane.showMessageDialog(null,"Не вся інформація про погоду була додана"); }
+
+    public void succsesfulAdding(){ JOptionPane.showMessageDialog(null,"Інформація про погоду була успішно додана"); }
+
 }
