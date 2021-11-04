dfd0ed364c2bf3cd5d0918aec0e5825bb3765c06
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-04 23:04:58 +0200

    adding checking if output of URL content is null

--- Server/src/dev/obniavka/Weather.java
+++ Server/src/dev/obniavka/Weather.java
@@ -4,28 +4,30 @@ import org.json.JSONObject;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class Weather {
 
-    public static ArrayList<String> findWeather(String city){
+    public static ArrayList<String> findWeather(String city) {
         ArrayList<String> info = new ArrayList<>();
         String output = URLContent.getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2cbefecfc08bdc704d243280d9a5599b");
         LocalDate localDate = LocalDate.now();
 
-        if(output == null){
+        if(Objects.equals(String.valueOf(output), "null")){
             return null;
         }
+
             JSONObject obj = new JSONObject(output);
             int tempr = (int) ((int) obj.getJSONObject("main").getDouble("temp") - 273.15);
-            int press = ((int)obj.getJSONObject("main").getDouble("pressure")-282);
+            int press = ((int) obj.getJSONObject("main").getDouble("pressure") - 282);
             String day = String.valueOf(localDate.getDayOfWeek());
 
-        info.add(String.valueOf(tempr));
-        info.add(String.valueOf(press));
-        info.add(day);
+            info.add(String.valueOf(tempr));
+            info.add(String.valueOf(press));
+            info.add(day);
 
-        return info;
+            return info;
+        }
 
-    }
 }
