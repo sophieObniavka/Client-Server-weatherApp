6c5ab36ecbb9b65a2dd050064548eec631989a97
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-06 00:49:34 +0200

    changing windows

--- ClientServerWeather/src/dev/obniavka/Main.java
+++ ClientServerWeather/src/dev/obniavka/Main.java
@@ -7,22 +7,31 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-    Stage window;
+    Stage windowMain;
     Scene scene1;
 
-    public static void main(String[] args){ Application.launch(args); }
+
+    public static void main (String[]args){
+        Application.launch(args);
+    }
 
     @Override
     public void start(Stage stage) throws Exception{
-        window = stage;
+
+        windowMain = stage;
+
         Parent root = FXMLLoader.load(getClass().getResource("scenes/sample.fxml"));
         stage.setHeight(831);
         stage.setWidth(1134);
         stage.setResizable(false);
         scene1 = new Scene(root);
-        window.setScene(scene1);
-        window.setTitle("Weather");
-        window.show();
+
+
+        windowMain.setScene(scene1);
+        windowMain.setTitle("Weather");
+        windowMain.show();
+
+
 
     }
 
