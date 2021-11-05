de3cec3ed6d6ecb79993affdfdd85aa64a53cc46
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-06 00:47:20 +0200

    changing method to get weather for all week

--- Server/src/dev/obniavka/Weather.java
+++ Server/src/dev/obniavka/Weather.java
@@ -1,33 +1,36 @@
 package dev.obniavka;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Objects;
 
 
 public class Weather {
 
-    public static ArrayList<String> findWeather(String city) {
-        ArrayList<String> info = new ArrayList<>();
-        String output = URLContent.getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2cbefecfc08bdc704d243280d9a5599b");
-        LocalDate localDate = LocalDate.now();
+    public static ArrayList<String> findForecast(String city) {
+        ArrayList<String> forecast = new ArrayList<>();
+        String outputToCheck = URLContent.getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2cbefecfc08bdc704d243280d9a5599b");
 
-        if(Objects.equals(String.valueOf(output), "null")){
+        String output = URLContent.getUrlContent("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=2cbefecfc08bdc704d243280d9a5599b");
+
+        if(Objects.equals(outputToCheck, "null")){
             return null;
         }
 
-            JSONObject obj = new JSONObject(output);
-            int tempr = (int) ((int) obj.getJSONObject("main").getDouble("temp") - 273.15);
-            int press = ((int) obj.getJSONObject("main").getDouble("pressure") - 282);
-            String day = String.valueOf(localDate.getDayOfWeek());
+        JSONObject obj = new JSONObject(output);
+        JSONArray weatherObject = obj.getJSONArray("list");
+        for (int i = 0; i < 40; i+=6) {
 
-            info.add(String.valueOf(tempr));
-            info.add(String.valueOf(press));
-            info.add(day);
+          forecast.add(String.valueOf(Integer.parseInt(weatherObject.getJSONObject(i).toString().substring(weatherObject.getJSONObject(i).toString().indexOf("temp") + 4,weatherObject.getJSONObject(i).toString().indexOf("feels")).replaceAll("\\D+", "").substring(0,3))-273));
+            forecast.add(String.valueOf(Integer.parseInt(weatherObject.getJSONObject(i).toString().substring(weatherObject.getJSONObject(i).toString().indexOf("pressure") + 4,weatherObject.getJSONObject(i).toString().indexOf("sea_level")).replaceAll("\\D+", ""))-282));
+            forecast.add(weatherObject.getJSONObject(i).toString().substring(weatherObject.getJSONObject(i).toString().indexOf("main") + 6,weatherObject.getJSONObject(i).toString().indexOf("id")).replace(",", "").replace("\"", ""));
 
-            return info;
         }
 
+
+
+        return forecast;
+    }
 }
