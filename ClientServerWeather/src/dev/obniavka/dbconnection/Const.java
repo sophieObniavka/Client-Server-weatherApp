36c70a4a9876d6a0b1487f480a4771844b5d0241
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-06 17:01:10 +0200

    moving package with MySQL connection to Server project

--- ClientServerWeather/src/dev/obniavka/dbconnection/Const.java
+++
@@ -1,7 +0,0 @@
-package dev.obniavka.dbconnection;
-
-public class Const {
-    public static String FORECAST_DAY =  "day";
-    public static String FORECAST_TEMP = "temp";
-    public static String FORECAST_PRESSURE = "pressure";
-}
