333d514a3d0961f3b9974c25900352549311aefb
IPZ_1_Obniavka_2021 <sopobn@gmail.com>
2021-11-19 16:37:21 +0200

    setting timer if admin is not active for some time

--- ClientServerWeather/src/dev/obniavka/controllers/WeatherInput.java
+++ ClientServerWeather/src/dev/obniavka/controllers/WeatherInput.java
@@ -17,6 +17,8 @@ import javafx.util.Duration;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class WeatherInput {
@@ -45,137 +47,214 @@ ConnectionToServer cnT = new ConnectionToServer();
     public WeatherInput() throws IOException {
     }
 
-    @FXML
-    void initialize(){
-
-
-        skySelect.setValue("Небо");
-        skySelect.setItems(skyList);
-        cityCheck.setValue("Місто, область");
-        cityCheck.setItems(cityList);
-        session.setVisible(false);
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        if(cityCheck.getValue().equals("Місто, область") && writeTemp.getText().isEmpty() && skySelect.getValue().equals("Небо") && writePress.getText().isEmpty()) {
-                            dateInput.setDisable(true);
-                            writeTemp.setDisable(true);
-                            skySelect.setDisable(true);
-                            cityCheck.setDisable(true);
-                            writePress.setDisable(true);
-                            back.setDisable(true);
-                            add.setDisable(true);
-                            press.setDisable(true);
-                            temp.setDisable(true);
-                            title.setDisable(true);
-                            session.setDisable(false);
-                            session.setVisible(true);
-                        }
-
-                    }
-                },
-                5000
-        );
-
-
-    }
-
-    public void setDateInput() {
-        LocalDate ld = dateInput.getValue();
-        LocalDate local = LocalDate.now();
-
-        if (ld.isAfter(local)) {
-            info.setVisible(true);
-            PauseTransition visiblePause = new PauseTransition(
-                    Duration.seconds(3)
-            );
-            visiblePause.setOnFinished(
-                    eve -> info.setVisible(false)
-            );
-            visiblePause.play();
-            dateInput.isDisable();
-            dateInput.getEditor().clear();
-            dateInput.setDisable(false);
-
-        }
-    }
 
 
 
 
-    public void turnBackToNow(ActionEvent actionEvent) throws IOException {
+        @FXML
+        void initialize () {
 
-        Parent newScene = FXMLLoader.load(getClass().getResource("/dev/obniavka/scenes/sample.fxml"));
-        Scene scene = new Scene(newScene);
-        window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.show();
-    }
-
-    public void addWeather(){
-        if (writePress.getText() == null || writeTemp.getText().isEmpty() || dateInput.getValue() == null || skySelect.getValue() == null || cityCheck.getValue() == null || writePress.getText() == null || skySelect.getValue() == "Небо" || cityCheck.getValue() == "Місто, область") {
-
-            inf.weatherAdd();
-        } else {
-            cnT.addWeather(cityCheck.getValue().toString(), writeTemp.getText(), writePress.getText(), String.valueOf(dateInput.getValue()),skySelect.getValue().toString());
-            inf.succsesfulAdding();
-        }
-
-        writePress.clear();
-        writeTemp.clear();
-        dateInput.setValue(null);
 
             skySelect.setValue("Небо");
+            skySelect.setItems(skyList);
             cityCheck.setValue("Місто, область");
+            cityCheck.setItems(cityList);
+            writeTemp.setText("");
+            writePress.setText("");
+            session.setVisible(false);
+            Timer t = new Timer( );
+            t.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+
+                    String city = cityCheck.getValue();
+                    String pressB = writePress.getText();
+                    String tempB = writeTemp.getText();
+                    String sky = skySelect.getValue();
+                    LocalDate date = dateInput.getValue();
+
+                    new java.util.Timer().schedule(
+                            new java.util.TimerTask() {
 
 
-    }
-    public void turnBackToLogin(ActionEvent actionEvent) throws IOException {
+                                @Override
+                                public void run() {
 
-        Parent newScene = FXMLLoader.load(getClass().getResource("/dev/obniavka/scenes/login.fxml"));
-        Scene scene = new Scene(newScene);
-        window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.show();
-    }
+                                    if (cityCheck.getValue().equals(city) && writePress.getText().equals(pressB) && writeTemp.getText().equals(tempB) && skySelect.getValue().equals(sky) && (date == null ? dateInput.getValue() == null : date.equals(dateInput.getValue()))) {
 
-    public void continueSession(){
-        session.setVisible(false);
-        dateInput.setDisable(false);
-        writeTemp.setDisable(false);
-        skySelect.setDisable(false);
-        cityCheck.setDisable(false);
-        writePress.setDisable(false);
-        back.setDisable(false);
-        add.setDisable(false);
-        press.setDisable(false);
-        temp.setDisable(false);
-        title.setDisable(false);
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
 
-                        if(cityCheck.getValue().equals("Місто, область") && writeTemp.getText().isEmpty() && skySelect.getValue().equals("Небо") && writePress.getText().isEmpty()) {
-                            dateInput.setDisable(true);
-                            writeTemp.setDisable(true);
-                            skySelect.setDisable(true);
-                            cityCheck.setDisable(true);
-                            writePress.setDisable(true);
-                            back.setDisable(true);
-                            add.setDisable(true);
-                            press.setDisable(true);
-                            temp.setDisable(true);
-                            title.setDisable(true);
-                            session.setDisable(false);
-                            session.setVisible(true);
-                        }
+                                        dateInput.setDisable(true);
+                                        writeTemp.setDisable(true);
+                                        skySelect.setDisable(true);
+                                        cityCheck.setDisable(true);
+                                        writePress.setDisable(true);
+                                        back.setDisable(true);
+                                        add.setDisable(true);
+                                        press.setDisable(true);
+                                        temp.setDisable(true);
+                                        title.setDisable(true);
+                                        session.setDisable(false);
+                                        session.setVisible(true);
+                                    }
 
-                    }
-                },
-                5000
-        );
-    }
+                                }
+                            },
+                            2000000
+                    );
+
+                }
+            }, 2100000,2100000);
+
+
+        }
+
+        public void setDateInput () {
+            LocalDate ld = dateInput.getValue();
+            LocalDate local = LocalDate.now();
+
+            if (ld.isAfter(local)) {
+                info.setVisible(true);
+                PauseTransition visiblePause = new PauseTransition(
+                        Duration.seconds(3)
+                );
+                visiblePause.setOnFinished(
+                        eve -> info.setVisible(false)
+                );
+                visiblePause.play();
+                dateInput.isDisable();
+                dateInput.getEditor().clear();
+                dateInput.setDisable(false);
+
+            }
+        }
+
+
+        public void turnBackToNow (ActionEvent actionEvent) throws IOException {
+
+            Parent newScene = FXMLLoader.load(getClass().getResource("/dev/obniavka/scenes/sample.fxml"));
+            Scene scene = new Scene(newScene);
+            window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+            window.setOnCloseRequest(e -> System.exit(1));
+        }
+
+        public void addWeather () {
+            if (writePress.getText() == null || writeTemp.getText().isEmpty() || dateInput.getValue() == null || skySelect.getValue() == null || cityCheck.getValue() == null || writePress.getText() == null || skySelect.getValue() == "Небо" || cityCheck.getValue() == "Місто, область") {
+
+                inf.weatherAdd();
+            } else {
+                cnT.addWeather(cityCheck.getValue(), writeTemp.getText(), writePress.getText(), String.valueOf(dateInput.getValue()), skySelect.getValue());
+                inf.succsesfulAdding();
+            }
+
+            writePress.clear();
+            writeTemp.clear();
+            dateInput.setValue(null);
+
+            Timer t = new Timer( );
+            t.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+
+                    String city = cityCheck.getValue();
+                    String pressB = writePress.getText();
+                    String tempB = writeTemp.getText();
+                    String sky = skySelect.getValue();
+                    LocalDate date = dateInput.getValue();
+                    new java.util.Timer().schedule(
+                            new java.util.TimerTask() {
+
+                                @Override
+                                public void run() {
+
+                                    if (cityCheck.getValue().equals(city) && writePress.getText().equals(pressB) && writeTemp.getText().equals(tempB) && skySelect.getValue().equals(sky) && (date == null ? dateInput.getValue() == null : date.equals(dateInput.getValue()))) {
+
+
+                                        dateInput.setDisable(true);
+                                        writeTemp.setDisable(true);
+                                        skySelect.setDisable(true);
+                                        cityCheck.setDisable(true);
+                                        writePress.setDisable(true);
+                                        back.setDisable(true);
+                                        add.setDisable(true);
+                                        press.setDisable(true);
+                                        temp.setDisable(true);
+                                        title.setDisable(true);
+                                        session.setDisable(false);
+                                        session.setVisible(true);
+                                    }
+
+                                }
+                            },
+                            2000000
+                    );
+
+                }
+            }, 2100000,2100000);
+
+        }
+        public void turnBackToLogin (ActionEvent actionEvent) throws IOException {
+
+            Parent newScene = FXMLLoader.load(getClass().getResource("/dev/obniavka/scenes/login.fxml"));
+            Scene scene = new Scene(newScene);
+            window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+            window.setOnCloseRequest(e -> System.exit(1));
+        }
+
+        public void continueSession () {
+            session.setVisible(false);
+            dateInput.setDisable(false);
+            writeTemp.setDisable(false);
+            skySelect.setDisable(false);
+            cityCheck.setDisable(false);
+            writePress.setDisable(false);
+            back.setDisable(false);
+            add.setDisable(false);
+            press.setDisable(false);
+            temp.setDisable(false);
+            title.setDisable(false);
+
+            Timer t = new Timer( );
+            t.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+
+                    String city = cityCheck.getValue();
+                    String pressB = writePress.getText();
+                    String tempB = writeTemp.getText();
+                    String sky = skySelect.getValue();
+                    LocalDate date = dateInput.getValue();
+                    new java.util.Timer().schedule(
+                            new java.util.TimerTask() {
+
+                                @Override
+                                public void run() {
+
+                                    if (cityCheck.getValue().equals(city) && writePress.getText().equals(pressB) && writeTemp.getText().equals(tempB) && skySelect.getValue().equals(sky) && (date == null ? dateInput.getValue() == null : date.equals(dateInput.getValue()))) {
+                                        dateInput.setDisable(true);
+                                        writeTemp.setDisable(true);
+                                        skySelect.setDisable(true);
+                                        cityCheck.setDisable(true);
+                                        writePress.setDisable(true);
+                                        back.setDisable(true);
+                                        add.setDisable(true);
+                                        press.setDisable(true);
+                                        temp.setDisable(true);
+                                        title.setDisable(true);
+                                        session.setDisable(false);
+                                        session.setVisible(true);
+                                    }
+
+                                }
+                            },
+                            2000000
+                    );
+
+                }
+            }, 2100000,2100000);
+        }
 
 }
